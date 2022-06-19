b74dcb58713e0208eb0017f2b8f4e653d3e34d84
halikali <tunchalikali@gmail.com>
2022-06-19 20:50:12 +0300

    complete character card component

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
+
 import style from "./CharacterCard.module.css";
 
-const CharacterCard = () => {
+const CharacterCard = ({ character }) => {
+  const { name, image, status, id } = character;
+
   return (
-    <div className={style.characterCard}>
-      <img
-        src="https://images.unsplash.com/photo-1655338434907-0de0591d7665?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-        alt="character"
-        className={style.avatar}
-      />
-      <p className={style.characterName}>Character Name</p>
-    </div>
+    <Link
+      to={`/details/${name.split(" ").join("+")}-${id}`}
+      className={style.characterCard}
+      style={{ opacity: status !== "Alive" && 0.5 }}
+    >
+      <img src={image} alt="character" className={style.avatar} />
+      <p className={style.characterName}>{name}</p>
+    </Link>
   );
 };
 
